dee77267df69c56e704cec49bc29bd7a0359615e
ghazialig <56931838+ghazialig@users.noreply.github.com>
2019-10-23 23:48:17 +0530

    Add files via upload

---
+++ Q6_Leap Year.py
@@ -0,0 +1,14 @@
+#Qustion:    Python program to find Year is Leap year or not
+
+while 1:
+    year = input("Enter a year to check leap year :")
+    try:
+        year = int(year)
+        if year%4 == 0 or year%100 == 0 or year%400 == 0:
+            print(year," is a leap year")
+        else:
+            print(year," is not a leap year")
+        break
+    except ValueError:
+        print("Enter year in number only.")
+        continue
